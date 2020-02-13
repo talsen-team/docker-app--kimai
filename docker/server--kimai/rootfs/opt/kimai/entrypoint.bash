2c6f1d3dbd7dca5628252a9d84f1e5da539259ce
David Lechner <david.lechner@talsen.team>
2020-02-14 00:39:01 +0100

    add docker build configuration

---
+++ docker/server--kimai/rootfs/opt/kimai/entrypoint.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function run_entrypoint() {
+    local \
+    DEFAULT_DIR="/default"
+
+    local \
+    KIMAI_VAR_DIR="/opt/kimai/var"
+
+    if [ -z "$( ls ${KIMAI_VAR_DIR} )" ];
+    then
+        sudo \
+        rsync \
+        --archive \
+        "${DEFAULT_DIR}/${KIMAI_VAR_DIR}/" \
+        "${KIMAI_VAR_DIR}"
+    fi
+
+    sudo \
+    chown \
+    --recursive \
+    www-data:www-data \
+    "${KIMAI_VAR_DIR}"
+
+    /startup.sh
+}
+
+run_entrypoint
